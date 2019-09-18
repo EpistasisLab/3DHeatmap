7e5ab07a44cfa5cba7c8fb9a6fea45f14d4e63fa
Michael Stauffer <mgstauff@gmail.com>
2019-09-18 18:02:19 -0400

    Update license info in Graph.cs Update Version to 1.0.0

--- Assets/3DHM/Scripts/Graph.cs
+++ Assets/3DHM/Scripts/Graph.cs
@@ -14,21 +14,13 @@ using TMPro;
 public class Graph : MonoBehaviorSingleton<Graph>
 {
     /*
-	Portions of this file originally coded by Douglas P. Hill, Copyright (C) 2010  Dartmouth College
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-    */
+     * Copyright (C) 2019 University of Pennsylvania.
+     * Lead Developer: Michael Stauffer (stauffer@upenn.edu)
+     * This source file and the whole 3D Heatmap project is licensed under the MIT License.
+     * See the LICENSE file in the project root directory for details
+     * 
+     * Portions of this file originally coded by Douglas P. Hill, Copyright (C) 2010  Dartmouth College
+     */
 
     /// <summary> Scene Object to conatain graph elements so they can be manipulated together </summary>
     public GameObject graphContainer;
