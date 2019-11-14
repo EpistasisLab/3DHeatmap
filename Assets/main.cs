9c08ef594b0e482ffe5edb08fba8efdec377248a
Michael Stauffer <mgstauff@gmail.com>
2019-11-14 12:11:20 -0500

    Add VRTK4 test scene

---
+++ Assets/main.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//VRTK testing
+
+public class main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void testcallback()
+    {
+        Debug.Log(" --- made it to testcallback ---");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
